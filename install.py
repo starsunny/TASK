6d712f39b762d891d38edbe14416854f41158f2c
starsunny <klalsunny@gmail.com>
2017-02-26 19:12:07 +0530

    task

---
+++ install.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os
+
+os.system("sudo apt-get update ")
+os.system("sudo apt-get install nginx -y")
+
+os.system("sudo apt-get install mysql-server  -y")
+
+os.system("sudo apt-get install php-fpm php-mysql -y")
+
